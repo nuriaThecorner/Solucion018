5ebf7057bfec918aa56272e6cc2b1f8eb3f26786
Nuria García <nuriag023@thecorner.cat>
2022-09-08 14:00:29 +0100

    CRUD

--- Datos/Class1.cs
+++ Datos/Class1.cs
@@ -1,12 +1,114 @@
-﻿using System;
+﻿using Datos.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datos
-{
+{ 
     public class Class1
     {
+        public static NorthWindTuneadoDbContext _dbContext = null;
+
+        //constructor
+        public Class1()
+        {
+            if (_dbContext == null)
+            {
+                _dbContext = new NorthWindTuneadoDbContext();
+            }
+        }
+
+        //metodo
+        public bool Create (Categoria categoria)
+        {
+            _dbContext.Categoria.Add(categoria);
+            return true;
+        }
+
+
+        //metodo
+        public bool GuardarCambios()
+        {
+            bool ok = false;
+
+            try
+            {
+                int resultado = 0;
+                resultado = _dbContext.SaveChanges();
+                if (resultado > 0)
+                {
+                    ok = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return ok;
+        }
+
+        public IList<Categoria> Read(int? id)
+        {
+            
+            IList<Categoria> categorias = null; 
+            categorias = _dbContext.Categoria.ToList();
+
+            if (id != null && id > 0)
+            {
+            categorias= categorias.Where(x=> x.CategoryID == id).ToList();
+            }
+
+
+            return categorias;
+
+        }
+
+
+        public bool Update (int id, string texto)
+        {
+
+            if (id != null && id > 0)
+            {
+                Categoria categoria = _dbContext.Categoria.Where(x => x.CategoryID == id).FirstOrDefault();
+                categoria.CategoryName = texto;
+                return true;
+            }
+
+
+            return false;
+
+        }
+
+
+
+        public bool Delete(int id, string texto)
+        {
+
+            if (id > 0)
+            {
+                Categoria categoria = _dbContext.Categoria.Where(x => x.CategoryID == id).FirstOrDefault();
+                _dbContext.Categoria.Remove(categoria);
+                return true;
+            }
+            else
+            {
+                IList<Categoria> categorias = null;
+                categorias = _dbContext.Categoria.ToList();
+                categorias = categorias.Where(x => x.CategoryName == texto).ToList();
+                foreach (Categoria categoria in categorias)
+                    {
+                        _dbContext.Categoria.Remove(categoria);
+                    }
+                return true;
+            }
+
+            
+
+        }
+
+
     }
 }
--- Principal/Program.cs
+++ Principal/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Datos;
+using Datos.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,159 @@ namespace Principal
 {
     internal class Program
     {
+        //declarar objeto
+        public static Class1 _class1 = null;
         static void Main(string[] args)
         {
+            
+            //objeto
+            Program program = null;
+            program = new Program();
+
+            //inicializar
+            _class1 = new Class1();
+
+            //CRUD
+
+            //llamar metodo
+            program.Create(); 
+            program.Read();
+            program.Update();
+            program.Delete();
+
         }
+
+        //Metodo
+        private void Create()
+        {
+            Categoria categoria = null;
+            categoria = new Categoria();
+
+            Console.WriteLine("Dime el nombre del nuevo registro");
+            string texto = Console.ReadLine();
+            if (texto != null)
+            {
+                categoria.CategoryName = texto;
+            }
+
+            Console.WriteLine("Dime la descripcion del nuevo registro");
+            texto = Console.ReadLine();
+            if (texto != null)
+            {
+                categoria.Description = texto;
+            }
+
+
+            //categoria.CategoryName = "Chisme";
+            //categoria.Description = "Utensilio cuya utilidad no se ha demostrado de momento";
+
+            bool ok = false;
+            ok = _class1.Create(categoria);
+            ok = _class1.GuardarCambios();
+
+            Console.WriteLine("El resultado de create a sido: " + ok);
+
+            Console.ReadLine();
+        }
+
+
+        private void Read()
+        {
+            IList<Categoria> categorias = null;
+
+            Console.WriteLine("Dime el id del registro a mostrar");
+            string texto = Console.ReadLine();
+            if (texto == null || texto == "")
+            {
+                categorias = _class1.Read(null);
+            }
+            else
+            {
+                int id = 0;
+                if (int.TryParse(texto, out id) == true)
+                {
+                    id = int.Parse(texto);
+                }
+                categorias = _class1.Read(id);
+            }
+
+            foreach (Categoria categoria in categorias)
+            {
+                Console.WriteLine(categoria.CategoryID + " " + categoria.CategoryName);
+            }
+            Console.ReadLine();
+        }
+
+
+
+        private void Update()
+        {
+            Console.WriteLine("Dime el ID del registro a modificar");
+            string texto = Console.ReadLine();
+            if (texto != null && texto != "")
+            {
+                int id = 0;
+                if (int.TryParse(texto, out id) == true)
+                {
+                    id = int.Parse(texto);
+                    Console.WriteLine("Dime el nuevo NOMBRE del registro");
+                    texto = Console.ReadLine();
+                }
+                if (texto != null && texto != "")
+                {
+                    bool ok = false;
+                    ok = _class1.Update(id, texto);
+                    if (ok == true)
+                    {
+                        ok = _class1.GuardarCambios();
+                    }
+                    Console.WriteLine("El resultado de Update ha sido: " + ok);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+
+
+        private void Delete()
+        {
+            Console.WriteLine("Dime el ID o el nombre del registro a eliminar");
+            string texto = Console.ReadLine();
+            if (texto != null && texto != "")
+            {
+                int id = 0;
+                if (int.TryParse(texto, out id) == true)
+                {
+                    id = int.Parse(texto);
+                    
+                }
+                
+                
+                  
+                    bool ok = false;
+                    ok = _class1.Delete(id, texto);
+                    if (ok == true)
+                    {
+                        ok = _class1.GuardarCambios();
+                    }
+                    Console.WriteLine("El resultado de Update ha sido: " + ok);
+                    Console.ReadLine();
+                  
+                    
+                
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
